b754601203f16686672a1715148dad5289913fa0
liuxinfeng <lxf_dream@qq.com>
2022-01-20 17:28:50 +0800

    1

---
+++ day01/HelloWorld.java
@@ -0,0 +1,13 @@
+public class HelloWorld {
+    // public只能定义一个和文件名相同的
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        System.out.println();
+        System.out.println("Hello");
+    }
+}
+
+
+// 注释有//单行注释 /**/多行注释 内容不会输出 /***/多行注释 内容可输出
+
+// println先打印后转换 print打印
